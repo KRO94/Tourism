7d95fd7757bbac0060b37bb82f8d7d56df1309d9
Roman Krovitskiy <roman.krovitskiy@gmail.com>
2017-08-15 03:28:58 +0300

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,37 @@
+		<footer>
+			<div class="topBlock">
+				<div class="container">
+					<div class="block block_1">
+						<div class="mini-title">ПРо нас</div>
+						<p>Спільнота однодумців, любителів мандрів, пригод та активного відочинку. Це Клуб друзів для друзів, де панує тепла атмосфера. Кожен учасник спільноти може обрати мандрівку за смаком: походи в гори, відпочинок на морі, екскурсії, печери, сплави, поїздки за кордон.</p>
+					</div>
+					<div class="block block_2">
+						<div class="mini-title">РОЗДІЛИ</div>
+						<ul class="menu">
+							<li><a href="#">Поїздки</a></li>
+							<li><a href="#">Регіони</a></li>
+							<li><a href="#">Відгуки</a></li>
+							<li><a href="#">Про нас</a></li>
+							<li><a href="#">Контакти</a></li>
+						</ul>
+					</div>
+					<div class="block block_3">
+						<div class="mini-title">КОНТАКТИ</div>
+						<span><i class="fa fa-map-marker" aria-hidden="true"></i> Киїів, вул. Соборна, 7</span>
+						<span><i class="fa fa-phone" aria-hidden="true"></i> +38 (066) 236 25 66</span>
+						<span><i class="fa fa-envelope-o" aria-hidden="true"></i> mail@travel.com</span>
+						<ul class="social">
+							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="bottomBlock">
+				<div class="container">© 2011-2017  Всі права захищено</div>
+			</div>
+		</footer>
 		<script src="js/jquery.min.js"></script>
 		<script src="js/jquery.fancybox.min.js"></script>
 		<script src="js/script.js"></script>
